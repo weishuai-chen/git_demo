5092bd10e3795ca4f28247fc25850cdf72e9f6d8
weishuai <18361242868@163.com>
2024-10-26 11:06:22 +0800

    function 1 第1次提交

--- src/main/java/com/chen/git_demo/controller/TestController.java
+++ src/main/java/com/chen/git_demo/controller/TestController.java
@@ -3,5 +3,10 @@ package com.chen.git_demo.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("dev分支，项目结构初始化");
+        function1();
+    }
+
+    public static void function1() {
+        System.out.println("function 1：第1次提交");
     }
 }
